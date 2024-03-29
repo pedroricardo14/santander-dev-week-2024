1625ef166f6bc24e641d245759be3b3241e66af6
pedroricardo14 <pedroricardo1032@gmail.com>
2024-03-29 10:04:29 -0300

    Starting back-end

---
+++ sdw2024/src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
@@ -0,0 +1,41 @@
+package me.dio.sdw24.adapters.out;
+
+import me.dio.sdw24.domain.model.Champions;
+import me.dio.sdw24.domain.ports.ChampionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ChampionsJdbcRepository implements ChampionsRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private final RowMapper<Champions> rowMapper;
+
+    public ChampionsJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.rowMapper = (rs, rowNum) -> new Champions(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("role"),
+                rs.getString("lore"),
+                rs.getString("image_url")
+        );
+    }
+
+    @Override
+    public List<Champions> findAll() {
+        return jdbcTemplate.query("SELECT * FROM CHAMPIONS", rowMapper);
+    }
+
+    @Override
+    public Optional<Champions> findById(Long id) {
+        String sql = "SELECT * FROM CHAMPIONS WHERE ID = ?";
+        Champions champion = jdbcTemplate.queryForObject(sql, rowMapper, id);
+        return Optional.ofNullable(champion);
+    }
+}
